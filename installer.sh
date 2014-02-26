6ac9184bfd6b633b39dabf310e41eb1365afb16f
Revers <revers@Jobuzzle.loc>
2014-02-26 01:08:30 +0100

    installer

--- installer.sh
+++ installer.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-tar -xf old-vim.tar ~/.vim ~/.vimrc
 
-tar -xf sv.tar
-mv .vimrc ~/
-mv .vim ~/
+echo "Sauvegarde de vos anciennes configuration Vim : ~/old-conf-vim.tar"
+(cd && tar -cf old-conf-vim.tar .vim .vimrc && rm -rf .vim .vimrc)
 
-wget http://norminator.clem.org/norminator
-chmod +x norminator
-mv norminator ~/shells/
+echo "Installation du Sublivim"
+tar -xf sv.tar && mv .vimrc ~/ && mv .vim ~/
 
+echo "Téléchargement de norminator"
+chmod +x ~/.vim/shells/norminator
+~/.vim/shells/norminator --auth
+
+cat ~/.norminator | head -n 1 > ~/.norminator_bis
+cat ~/.norminator | tail -n 1 | cut -d ' ' -f -5 | tr -d '\n' >> ~/.norminator_bis
+echo " expires=\"4242-01-01 00:00:00Z\"; " | tr -d '\n' >> ~/.norminator_bis
+cat ~/.norminator | tail -n 1 | cut -d ' ' -f 8- >> ~/.norminator_bis
+mv ~/.norminator_bis ~/.norminator
+chmod 444 ~/.norminator
