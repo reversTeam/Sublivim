61681cfb30c5a74168e4b5c36867a147616cb27b
ReversTeam <theotimeriviere@gmail.com>
2014-04-16 07:30:54 +0200

    Message par default

--- installer.sh
+++ installer.sh
@@ -5,7 +5,7 @@ SHELL_ACTIVE="${HOME}/.$(basename $SHELL)rc"
 cd ~
 if [ -d $HOME/.Sublivim ]; then
 	echo "Mise a jour du Sublivim"
-	(cd .Sublivim && git pull > /dev/null)
+	(cd .Sublivim && git pull > /dev/null 2> /dev/null)
 else
 	echo "Clone du depot"
 	git clone https://github.com/reversTeam/Sublivim.git .Sublivim
