7eff874a753c2ee478fb56e5ac12292d9a8dd9a0
amnacog <toto@mail.com>
2015-02-15 02:15:38 +0100

    Refix line 29 [installer.sh]

--- installer.sh
+++ installer.sh
@@ -1,14 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 SV_VERSION="0"
 SHELL_ACTIVE="${HOME}/.$(basename $SHELL)rc"
 
+null() {
+	"$@" >/dev/null 2>&1
+}
+
 cd ~
 if [ -d $HOME/.Sublivim ]; then
-	echo "Mise a jour du Sublivim" > /dev/null
-	(cd .Sublivim && git stash > /dev/null 2> /dev/null && git pull > /dev/null 2> /dev/null &)
+#	echo "Mise a jour du Sublivim"
+	(cd .Sublivim && null git stash && null git pull &)
 else
 	echo "Clone du depot"
-	git clone https://github.com/reversTeam/Sublivim.git .Sublivim
+	git clone https://github.com/reversTeam/Sublivim.git .Sublivim --progress
 
 	if [ -d $HOME/.vim ] && [ -f $HOME/.vimrc ]; then
 		echo "Sauvegarde de vos anciennes configurations Vim : ~/old-conf-vim.tar"
@@ -17,18 +21,18 @@ else
 	fi
 
 	echo "Installation du Sublivim"
-	ln -s .Sublivim/vimrc ~/.vimrc 
+	ln -s .Sublivim/vimrc ~/.vimrc
 	ln -s .Sublivim/vim ~/.vim
 fi
 if [ ! -e ~/.Sublivim/config_perso ]; then
 	echo "let g:syntastic_c_include_dirs = ['../../../include', '../../include','../include','./include']" > ~/.Sublivim/config_perso
 fi
 
-SHELLRC=`cat $SHELL_ACTIVE | grep "source ~/.Sublivim/sublivimrc.sh"`
+SBVRC="$HOME/.Sublivim/sublivimrc.sh"
 
-if [ "$SHELLRC" == "" ]; then
-	echo "source ~/.Sublivim/sublivimrc.sh" >> $SHELL_ACTIVE
-	source $HOME/.Sublivim/sublivimrc.sh
+if ! null cat $SHELL_ACTIVE | grep "source $SBVRC"; then
+	echo "source $SBVRC" >> $SHELL_ACTIVE
+	. $SBVRC
 fi
 
-cd - > /dev/null
+null cd -
